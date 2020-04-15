b1187bff323103b83ec80d97d48e744ee574cd18
Mika Sormunen <mika.sormunen@citrus.fi>
2020-04-15 19:02:30 +0300

    Project name to tombalo

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `tombalo arvontapalvelu`,
+        short_name: `tombalo`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
